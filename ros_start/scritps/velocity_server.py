fb0a2604f2edd24b096a643ec7015b750f2ba16c
Takashi Ogura <t.ogura@gmail.com>
2015-05-10 15:32:37 +0900

    Fix typo

--- ros_start/scritps/velocity_server.py
+++ ros_start/scritps/velocity_server.py
@@ -4,21 +4,21 @@ from geometry_msgs.msg import Twist
 from ros_start.srv import SetVelocity
 from ros_start.srv import SetVelocityResponse
 
-MAX_LINEAR_VELICITY = 1.0
-MIN_LINEAR_VELICITY = -1.0
-MAX_ANGULAR_VELICITY = 2.0
-MIN_ANGULAR_VELICITY = -2.0
+MAX_LINEAR_VELOCITY = 1.0
+MIN_LINEAR_VELOCITY = -1.0
+MAX_ANGULAR_VELOCITY = 2.0
+MIN_ANGULAR_VELOCITY = -2.0
 
 def velocity_handler(req):
     vel = Twist()
     is_set_success = True
-    if req.linear_velocity <= MAX_LINEAR_VELICITY and (
-            req.linear_velocity >= MIN_LINEAR_VELICITY):
+    if req.linear_velocity <= MAX_LINEAR_VELOCITY and (
+            req.linear_velocity >= MIN_LINEAR_VELOCITY):
         vel.linear.x = req.linear_velocity
     else:
         is_set_success = False
-    if req.angular_velocity <= MAX_ANGULAR_VELICITY and (
-            req.angular_velocity >= MIN_ANGULAR_VELICITY):
+    if req.angular_velocity <= MAX_ANGULAR_VELOCITY and (
+            req.angular_velocity >= MIN_ANGULAR_VELOCITY):
         vel.angular.z = req.angular_velocity
     else:
         is_set_success = False
