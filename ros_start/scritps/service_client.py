5d65b35623d2dbdb518a6e4a7f95ec224bf879a1
yoshimalucky <y-tanaka@jsk.imi.i.u-tokyo.ac.jp>
2019-05-24 14:54:17 +0900

    Add initialization of the service client node.

--- ros_start/scritps/service_client.py
+++ ros_start/scritps/service_client.py
@@ -3,14 +3,20 @@
 import rospy
 from std_srvs.srv import Empty
 
-def service_client():
+def call_service():
     rospy.loginfo('waiting service')
     rospy.wait_for_service('call_me')
+
     try:
         service = rospy.ServiceProxy('call_me', Empty)
         response = service()
     except rospy.ServiceException, e:
         print "Service call failed: %s" % e
+
+def service_client():
+    rospy.init_node('service_client')
+    call_service()
+    rospy.spin()
 
 if __name__ == "__main__":
     service_client()
